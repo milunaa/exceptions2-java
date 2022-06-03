cd4f77a67dc46486a2a774561135621a7287d257
milunaa <milenalunadosantos@gmail.com>
2022-06-03 10:13:30 -0300

    Good solution

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,7 +2,9 @@ package application;
 
 import java.util.Locale;
 import java.util.Scanner;
+
 import entities.Account;
+import exceptions.BusinessException;
 
 public class Program {
 
@@ -28,13 +30,12 @@ public class Program {
 		System.out.print("Informe uma quantia para sacar: ");
 		double amount = sc.nextDouble();
 		
-		String error = acc.validateWithdraw(amount);
-		if (error != null) {
-			System.out.println(error);
-		}
-		else {
+		try {
 			acc.withdraw(amount);
 			System.out.printf("Novo saldo: %.2f%n", acc.getBalance());
+		}
+		catch (BusinessException e) {
+			System.out.println(e.getMessage());
 		}
 		
 		sc.close();
--- src/entities/Account.java
+++ src/entities/Account.java
@@ -1,5 +1,7 @@
 package entities;
 
+import exceptions.BusinessException;
+
 public class Account {
 	
 	private Integer number;
@@ -54,16 +56,16 @@ public class Account {
 	}
 	
 	public void withdraw(double amount) {
+		validateWithdraw(amount);
 		balance = balance - amount;
 	}
 	
-	public String validateWithdraw(double amount) {
+	private void validateWithdraw(double amount) {
 		if (amount > getWithdrawLimit()) {
-			return "Erro de saque: A quantia excede o limite de saque";
+			throw new BusinessException("Erro de saque: A quantia excede o limite de saque");
 		}
 		if (amount > getBalance()) {
-			return "Error de saque: Saldo insuficiente";
+			throw new BusinessException("Error de saque: Saldo insuficiente");
 		}
-		return null;
 	}
 }
